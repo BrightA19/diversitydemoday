6a75cd3233bea5886c145ec83880982cbf2df78d
Bright Amoateng <bright.amoateng150@gmail.com>
2021-08-09 15:04:28 -0400

    Add bubbles effect

--- js/script.js
+++ js/script.js
@@ -63,3 +63,74 @@ setInterval(() => {
 
 // Fix countdown if user navigates elsewhere
 window.onfocus = () => countdown = eventStartDate - new Date();
+
+
+
+const cvs = document.querySelector("#cvs_effects");
+const ctx = cvs.getContext("2d");
+
+function resizeCanvas() {
+    cvs.width = innerWidth;
+    cvs.height = innerHeight;
+}
+
+window.onresize = resizeCanvas
+resizeCanvas();
+
+const FRAMES_PER_SECOND = 20;
+const ADD_CIRCLE_DELAY = 500;
+
+let timeSinceNewCircle = Date.now();
+let circles = [];
+
+class Circle {
+    constructor() {
+        this.size = 2 + Math.random() * 18;
+    
+        this.x = Math.random() * cvs.width;
+        this.y = cvs.height + this.size;
+        this.speed = 10 - (this.size/20) * 8;
+        this.delete = false;
+    }
+
+    update() {
+        this.y -= this.speed;
+        
+        if (this.y < -this.size) {
+            this.delete = true;
+        }
+    }
+
+    render() {
+        ctx.fillStyle = "rgba(249, 236, 255, 0.3)";
+
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+
+setInterval(() => {
+    let now = Date.now();
+    if (now - timeSinceNewCircle > ADD_CIRCLE_DELAY && circles.length < 40) {
+        circles.push(new Circle());
+        timeSinceNewCircle = now;
+    }
+
+
+    ctx.clearRect(0, 0, cvs.width, cvs.height);
+
+    for (let i = circles.length - 1; i >= 0; i--) {
+        let circle = circles[i];
+        circle.update();
+
+        if (circle.delete) {
+            circles.splice(i, 1);
+        }
+        else {
+            circle.render();
+        }
+    }
+
+}, 1000 / FRAMES_PER_SECOND);
